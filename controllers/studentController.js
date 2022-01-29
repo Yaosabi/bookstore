0651cdea1157fd56f34e5624210ab0027a17acb1
Yaosabi <lorenzoimurillo@outlook.com>
2022-01-28 19:08:23 -0800

    checkpoint 2

---
+++ controllers/studentController.js
@@ -0,0 +1,66 @@
+const {Student, Course} = require('../models')
+
+//view all
+module.exports.viewAll = async fucntion(req,res){
+    const students = await Student.findAll();
+    res.render('students/view_all', {students});
+}
+
+//profile
+module.exports.viewProfile = async function(req,res){
+    const student = await Student.findByPk(req.params.id);
+    res.render('student/profile', {student})
+}
+
+//render add
+module.exports.renderAddForm = function(req,res){
+    const student = {
+        first_name: '',
+        last_name:'',
+        grade_level: 9,
+    }
+    res.render('students/add', {student});
+}
+
+//add
+module.exports.addStudents = async function(req,res){
+    const students = await  Student.create({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        grade_level: req.body.grade_level
+    });
+    res.render(`/students/profile/${student.id}`)
+}
+
+//render edit
+module.exports.renderEditForm = async function(req,res){
+    const students = await Student.findByPk(req.params.id);
+    res.render('student/edit', {student});
+}
+
+//update
+module.exports.updateStudent = async function(req,res){
+    const student = await Student.update({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        grade_level: req.body.grade_level
+    }, {
+        where: {
+            id: req.params.id
+        }
+    });
+    res.redirect(`/students/profile/${req.params.id}`);
+}
+
+//edit
+
+
+//delete
+module.exports.deleteStudent = async function(req,res){
+    await Student.destroy({
+        where:{
+            id:req.params.id
+        }
+    });
+    res.redirect('/students');
+}
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const courseController = require('../controllers/courseController.js');
+const studentController = require('../controllers/studentController.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,4 +14,13 @@ router.get('/courses/edit/:id', courseController.renderEditForm);
 router.post('/courses/edit/:id', courseController.updateCourse);
 router.get('/courses/add', courseController.renderAddForm);
 router.get('/courses/delete/:id', courseController.deleteCourse);
+
+
+
+router.get('/students', studentController.viewAll);
+router.get('/students/profile/:id', studentController.viewProfile);
+router.get('/students/edit/:id', studentController.renderEditForm);
+router.get('/students/add', studentController.renderAddForm);
+router.post('/students/add', studentController.addStudents);
+router.get('/students/delete/:id', studentController.deleteStudent);
 module.exports = router;
